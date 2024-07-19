5ca130701c1a130e4e1d780704f332c666c3e130
SunM <coex0113@gmail.com>
2024-07-20 00:41:42 +0900

    KAN-208 Feat. implementing api call error page for RegisterSaasView

--- src/views/RegisterSaasView.vue
+++ src/views/RegisterSaasView.vue
@@ -3,7 +3,8 @@
   <div class="w-5/6 float-right">
     <header-breadcrumb></header-breadcrumb>
     <main>
-      <register-main></register-main>
+      <register-main v-if="true"></register-main>
+      <content-error v-else></content-error>
     </main>
   </div>
 
@@ -17,5 +18,6 @@ import SideNav from '@/components/SideNav.vue'
 import HeaderBreadcrumb from '@/components/HeaderBreadcrumb.vue'
 import TheFooter from '@/components/TheFooter.vue'
 import RegisterMain from '@/components/RegisterMain.vue'
+import ContentError from '@/components/ContentError.vue'
 
 </script>
