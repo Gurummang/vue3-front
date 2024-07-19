4f1a388b8fd588a612595cdfffbcf14632f2768f
SunM <coex0113@gmail.com>
2024-07-19 19:41:41 +0900

    KAN-208 Refactor. RegisterSaasView에서 script 부분을 script setup 형식으로 변경

--- src/views/RegisterSaasView.vue
+++ src/views/RegisterSaasView.vue
@@ -1,30 +1,21 @@
 <template>
-<side-nav class="w-1/6 float-left"></side-nav>
-<div class="w-5/6 float-right">
-  <header-breadcrumb></header-breadcrumb>
-  <main>
-    <register-main></register-main>
-  </main>
-</div>
+  <side-nav class="w-1/6 float-left"></side-nav>
+  <div class="w-5/6 float-right">
+    <header-breadcrumb></header-breadcrumb>
+    <main>
+      <register-main></register-main>
+    </main>
+  </div>
 
-<!-- <footer>
-  <the-footer></the-footer>
-</footer> -->
+  <!-- <footer>
+    <the-footer></the-footer>
+  </footer> -->
 </template>
 
-<script>
+<script setup>
 import SideNav from '@/components/SideNav.vue'
 import HeaderBreadcrumb from '@/components/HeaderBreadcrumb.vue'
 import TheFooter from '@/components/TheFooter.vue'
 import RegisterMain from '@/components/RegisterMain.vue'
 
-export default {
-  name: 'RegisterSaaS',
-  components: {
-    TheFooter,
-    SideNav,
-    HeaderBreadcrumb,
-    RegisterMain,
-  },
-}
 </script>
