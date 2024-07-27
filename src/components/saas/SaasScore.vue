7affaf55007f9a53871a297a753e3a484091bb05
SunM <coex0113@gmail.com>
2024-07-27 14:00:15 +0900

    KAN-245 Feat. 브랜치 변경

---
+++ src/components/saas/SaasScore.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="flex items-center justify-between p-1.5 mb-5 bg-white shadow-sm rounded-lg">
+      <div class="flex items-center space-x-4">
+        <h1 class="text-xl font-semibold text-gray-700 px-3 content-center">
+          <!-- <v-icon size="30">mdi-chevron-right</v-icon>  -->
+          {{ $route.name.slice($route.name.lastIndexOf(' ')) }}
+        </h1>
+      </div>
+      
+    </div>
+</template>
+
+<script setup>
+
+
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- src/views/SaasSlackView.vue
+++ src/views/SaasSlackView.vue
@@ -1,9 +1,10 @@
 <template>
   <side-nav class="w-1/6 float-left"></side-nav>
-  <div class="w-5/6 float-right">
+  <div class="w-5/6 float-right px-5">
     <header-breadcrumb></header-breadcrumb>
     <main>
-      <content-error></content-error>
+      <saas-score></saas-score>
+      <!-- <content-error></content-error> -->
     </main>
   </div>
 
@@ -17,8 +18,9 @@ import { ref } from 'vue'
 import SideNav from '@/components/SideNav.vue'
 import HeaderBreadcrumb from '@/components/HeaderBreadcrumb.vue'
 import TheFooter from '@/components/TheFooter.vue'
-
 import ContentError from '@/components/ContentError.vue'
+import SaasScore from '@/components/saas/SaasScore.vue'
+
 
 let responseData = ref(null);
 
