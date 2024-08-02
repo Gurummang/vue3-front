c03ea70621799fcea921a1baecdd3f7ef11f9565
SunM <coex0113@gmail.com>
2024-08-02 14:55:55 +0900

    KAN-287 Feat. API 값을 불러 올때 까지 로딩 표시

--- src/views/SaasSlackView.vue
+++ src/views/SaasSlackView.vue
@@ -2,13 +2,19 @@
   <side-nav class="w-1/6 float-left"></side-nav>
   <div class="w-5/6 float-right px-5">
     <header-breadcrumb></header-breadcrumb>
-    <main class="scroll-h scroll overflow-auto rounded-lg">
+    <cycle-loading
+      v-if="loading"></cycle-loading>
+    <main 
+      class="scroll-h scroll overflow-auto rounded-lg"
+      v-else>
       <div>
         <saas-score></saas-score>
         <saas-statistics
           :fileStatistics="fileStatistics"></saas-statistics>
         <div class="grid grid-cols-2 gap-5 mb-5">
-          <file-ratio-chart></file-ratio-chart>
+          <file-ratio-chart
+          :fileSize="fileSize"
+          ></file-ratio-chart>
           <recent-upload-list></recent-upload-list>
         </div>
         <users-top-5></users-top-5>
@@ -16,7 +22,6 @@
       <!-- <content-error></content-error> -->
     </main>
   </div>
-
   <!-- <footer>
     <the-footer></the-footer>
   </footer> -->
@@ -24,35 +29,47 @@
 
 <script setup>
 import { ref } from 'vue'
-import { getFileStatistics } from '@/apis/saas.js'
+import { getFileStatistics, getFileSize } from '@/apis/saas.js'
 import SideNav from '@/components/SideNav.vue'
 import HeaderBreadcrumb from '@/components/HeaderBreadcrumb.vue'
 import TheFooter from '@/components/TheFooter.vue'
 import ContentError from '@/components/ContentError.vue'
+import CycleLoading from '@/components/CycleLoading.vue'
 import SaasScore from '@/components/saas/SaasScore.vue'
 import SaasStatistics from '@/components/saas/SaasStatistics.vue'
 import FileRatioChart from '@/components/saas/FileRatioChart.vue'
 import RecentUploadList from '@/components/saas/RecentUploadList.vue'
 import UsersTop5 from '@/components/saas/UsersTop5.vue'
 
+let totalFiles = ref(0);
+let sensitiveFiles = ref(0);
+let maliciousFiles = ref(0);
+let connectedAccounts = ref(0);
+let fileSize = ref(null);
 let fileStatistics = ref(null);
+let loading = ref(true);
 
 const data = {
   "email": "hsp003636@gmail.com"
 }
 
-// Promise.all([
-//   getFileStatistics('slack', data)]).then((values) => {
-//   fileStatistics = values[0];
-//   console.log(values[0]);
-// });
+Promise.all([
+  getFileStatistics('slack', data),
+  getFileSize('slack', data)])
+  .then((values) => {
+  fileStatistics.value = values[0];
+  // totalFiles.value = values[0]['totalFiles'];
+  // sensitiveFiles.value = values[0]['sensitiveFiles'];
+  // maliciousFiles.value = values[0]['maliciousFiles'];
+  // connectedAccounts.value = values[0]['connectedAccounts'];
+  fileSize.value = values[1];
 
-getFileStatistics('slack', data).then((response) => {
-  fileStatistics.value = response;
-  // console.log(response);
+  console.log(values[0], values[1]);  
+  loading.value = false;
+  // console.log(totalFiles, sensitiveFiles, maliciousFiles, connectedAccounts);
 });
 
-console.log(fileStatistics);
+// console.log(fileStatistics);
 
 // console.log(fileStatistics);
 // let fileStatistics = getFileStatistics('slack', data);
