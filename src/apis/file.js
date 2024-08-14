94c0005007811f2d499d0ca8bff2c9f1250dd035
SunM <coex0113@gmail.com>
2024-08-14 16:24:05 +0900

    KAN-325 Feat. HistoryTrendsApi로 데이터 묶음

--- src/apis/file.js
+++ src/apis/file.js
@@ -38,19 +38,19 @@ let fileVtUploadApi = async (data) => {
   }
 };
 
-let historyStatisticsApi = async (data) => {
-  try {
-    // const response = await axios.post('/api/v1/files/board', data);
-    // if(response.status == '200') {
-    //   return response.data;
-    // }
-    const response = [20, 5, 10];
-    return response;
-  } catch (err) {
-    console.error('Error:', err);
-    throw err;  
-  }
-};
+// let historyStatisticsApi = async (data) => {
+//   try {
+//     // const response = await axios.post('/api/v1/files/board', data);
+//     // if(response.status == '200') {
+//     //   return response.data;
+//     // }
+//     const response = [20, 5, 10];
+//     return response;
+//   } catch (err) {
+//     console.error('Error:', err);
+//     throw err;  
+//   }
+// };
 
 let historyTrendsApi = async (data) => {
   try {
@@ -58,218 +58,227 @@ let historyTrendsApi = async (data) => {
     // if(response.status == '200') {
     //   return response.data;
     // }
-    const response = [
-      {
-          "date": "2024-07-16",
-          "uploadCount": 0,
-          "deletedCount": 0,
-          "modifyCount": 0,
-          "movedCount": 0
-      },
-      {
-          "date": "2024-07-17",
-          "uploadCount": 0,
-          "deletedCount": 0,
-          "modifyCount": 0,
-          "movedCount": 0
-      },
-      {
-          "date": "2024-07-18",
-          "uploadCount": 0,
-          "deletedCount": 0,
-          "modifyCount": 0,
-          "movedCount": 0
-      },
-      {
-          "date": "2024-07-19",
-          "uploadCount": 0,
-          "deletedCount": 0,
-          "modifyCount": 0,
-          "movedCount": 0
-      },
-      {
-          "date": "2024-07-20",
-          "uploadCount": 0,
-          "deletedCount": 0,
-          "modifyCount": 0,
-          "movedCount": 0
-      },
-      {
-          "date": "2024-07-21",
-          "uploadCount": 0,
-          "deletedCount": 0,
-          "modifyCount": 0,
-          "movedCount": 0
-      },
-      {
-          "date": "2024-07-22",
-          "uploadCount": 0,
-          "deletedCount": 0,
-          "modifyCount": 0,
-          "movedCount": 0
-      },
-      {
-          "date": "2024-07-23",
-          "uploadCount": 0,
-          "deletedCount": 0,
-          "modifyCount": 0,
-          "movedCount": 0
-      },
-      {
-          "date": "2024-07-24",
-          "uploadCount": 0,
-          "deletedCount": 0,
-          "modifyCount": 0,
-          "movedCount": 0
-      },
-      {
-          "date": "2024-07-25",
-          "uploadCount": 0,
-          "deletedCount": 0,
-          "modifyCount": 0,
-          "movedCount": 0
-      },
-      {
-          "date": "2024-07-26",
-          "uploadCount": 0,
-          "deletedCount": 0,
-          "modifyCount": 0,
-          "movedCount": 0
-      },
-      {
-          "date": "2024-07-27",
-          "uploadCount": 0,
-          "deletedCount": 0,
-          "modifyCount": 0,
-          "movedCount": 0
-      },
-      {
-          "date": "2024-07-28",
-          "uploadCount": 0,
-          "deletedCount": 0,
-          "modifyCount": 0,
-          "movedCount": 0
-      },
-      {
-          "date": "2024-07-29",
-          "uploadCount": 2,
-          "deletedCount": 0,
-          "modifyCount": 0,
-          "movedCount": 0
-      },
-      {
-          "date": "2024-07-30",
-          "uploadCount": 3,
-          "deletedCount": 0,
-          "modifyCount": 0,
-          "movedCount": 0
-      },
-      {
-          "date": "2024-07-31",
-          "uploadCount": 0,
-          "deletedCount": 0,
-          "modifyCount": 0,
-          "movedCount": 0
-      },
-      {
-          "date": "2024-08-01",
-          "uploadCount": 0,
-          "deletedCount": 0,
-          "modifyCount": 0,
-          "movedCount": 0
-      },
-      {
-          "date": "2024-08-02",
-          "uploadCount": 1,
-          "deletedCount": 0,
-          "modifyCount": 0,
-          "movedCount": 0
-      },
-      {
-          "date": "2024-08-03",
-          "uploadCount": 0,
-          "deletedCount": 0,
-          "modifyCount": 1,
-          "movedCount": 0
-      },
-      {
-          "date": "2024-08-04",
-          "uploadCount": 1,
-          "deletedCount": 2,
-          "modifyCount": 0,
-          "movedCount": 0
-      },
-      {
-          "date": "2024-08-05",
-          "uploadCount": 1,
-          "deletedCount": 0,
-          "modifyCount": 1,
-          "movedCount": 0
-      },
-      {
-          "date": "2024-08-06",
-          "uploadCount": 1,
-          "deletedCount": 1,
-          "modifyCount": 0,
-          "movedCount": 0
-      },
-      {
-          "date": "2024-08-07",
-          "uploadCount": 1,
-          "deletedCount": 0,
-          "modifyCount": 0,
-          "movedCount": 0
-      },
-      {
-          "date": "2024-08-08",
-          "uploadCount": 0,
-          "deletedCount": 0,
-          "modifyCount": 0,
-          "movedCount": 0
-      },
-      {
-          "date": "2024-08-09",
-          "uploadCount": 0,
-          "deletedCount": 0,
-          "modifyCount": 0,
-          "movedCount": 0
-      },
-      {
-          "date": "2024-08-10",
-          "uploadCount": 0,
-          "deletedCount": 0,
-          "modifyCount": 0,
-          "movedCount": 0
-      },
-      {
-          "date": "2024-08-11",
-          "uploadCount": 0,
-          "deletedCount": 0,
-          "modifyCount": 0,
-          "movedCount": 0
-      },
-      {
-          "date": "2024-08-12",
-          "uploadCount": 0,
-          "deletedCount": 0,
-          "modifyCount": 0,
-          "movedCount": 0
-      },
-      {
-          "date": "2024-08-13",
-          "uploadCount": 0,
-          "deletedCount": 0,
-          "modifyCount": 0,
-          "movedCount": 0
-      },
-      {
-          "date": "2024-08-14",
-          "uploadCount": 0,
-          "deletedCount": 0,
-          "modifyCount": 0,
-          "movedCount": 0
+    const response = {
+      "status": "success",
+      "data": {
+          "totalUpload": 15,
+          "totalDeleted": 3,
+          "totalChanged": 2,
+          "totalMoved": 0,
+          "fileHistoryStatistics": [
+              {
+                  "date": "2024-07-16",
+                  "uploadCount": 0,
+                  "deletedCount": 0,
+                  "modifyCount": 0,
+                  "movedCount": 0
+              },
+              {
+                  "date": "2024-07-17",
+                  "uploadCount": 0,
+                  "deletedCount": 0,
+                  "modifyCount": 0,
+                  "movedCount": 0
+              },
+              {
+                  "date": "2024-07-18",
+                  "uploadCount": 0,
+                  "deletedCount": 0,
+                  "modifyCount": 0,
+                  "movedCount": 0
+              },
+              {
+                  "date": "2024-07-19",
+                  "uploadCount": 0,
+                  "deletedCount": 0,
+                  "modifyCount": 0,
+                  "movedCount": 0
+              },
+              {
+                  "date": "2024-07-20",
+                  "uploadCount": 0,
+                  "deletedCount": 0,
+                  "modifyCount": 0,
+                  "movedCount": 0
+              },
+              {
+                  "date": "2024-07-21",
+                  "uploadCount": 0,
+                  "deletedCount": 0,
+                  "modifyCount": 0,
+                  "movedCount": 0
+              },
+              {
+                  "date": "2024-07-22",
+                  "uploadCount": 0,
+                  "deletedCount": 0,
+                  "modifyCount": 0,
+                  "movedCount": 0
+              },
+              {
+                  "date": "2024-07-23",
+                  "uploadCount": 0,
+                  "deletedCount": 0,
+                  "modifyCount": 0,
+                  "movedCount": 0
+              },
+              {
+                  "date": "2024-07-24",
+                  "uploadCount": 0,
+                  "deletedCount": 0,
+                  "modifyCount": 0,
+                  "movedCount": 0
+              },
+              {
+                  "date": "2024-07-25",
+                  "uploadCount": 0,
+                  "deletedCount": 0,
+                  "modifyCount": 0,
+                  "movedCount": 0
+              },
+              {
+                  "date": "2024-07-26",
+                  "uploadCount": 0,
+                  "deletedCount": 0,
+                  "modifyCount": 0,
+                  "movedCount": 0
+              },
+              {
+                  "date": "2024-07-27",
+                  "uploadCount": 0,
+                  "deletedCount": 0,
+                  "modifyCount": 0,
+                  "movedCount": 0
+              },
+              {
+                  "date": "2024-07-28",
+                  "uploadCount": 0,
+                  "deletedCount": 0,
+                  "modifyCount": 0,
+                  "movedCount": 0
+              },
+              {
+                  "date": "2024-07-29",
+                  "uploadCount": 2,
+                  "deletedCount": 0,
+                  "modifyCount": 0,
+                  "movedCount": 0
+              },
+              {
+                  "date": "2024-07-30",
+                  "uploadCount": 3,
+                  "deletedCount": 0,
+                  "modifyCount": 0,
+                  "movedCount": 0
+              },
+              {
+                  "date": "2024-07-31",
+                  "uploadCount": 0,
+                  "deletedCount": 0,
+                  "modifyCount": 0,
+                  "movedCount": 0
+              },
+              {
+                  "date": "2024-08-01",
+                  "uploadCount": 0,
+                  "deletedCount": 0,
+                  "modifyCount": 0,
+                  "movedCount": 0
+              },
+              {
+                  "date": "2024-08-02",
+                  "uploadCount": 1,
+                  "deletedCount": 0,
+                  "modifyCount": 0,
+                  "movedCount": 0
+              },
+              {
+                  "date": "2024-08-03",
+                  "uploadCount": 0,
+                  "deletedCount": 0,
+                  "modifyCount": 1,
+                  "movedCount": 0
+              },
+              {
+                  "date": "2024-08-04",
+                  "uploadCount": 1,
+                  "deletedCount": 2,
+                  "modifyCount": 0,
+                  "movedCount": 0
+              },
+              {
+                  "date": "2024-08-05",
+                  "uploadCount": 1,
+                  "deletedCount": 0,
+                  "modifyCount": 1,
+                  "movedCount": 0
+              },
+              {
+                  "date": "2024-08-06",
+                  "uploadCount": 1,
+                  "deletedCount": 1,
+                  "modifyCount": 0,
+                  "movedCount": 0
+              },
+              {
+                  "date": "2024-08-07",
+                  "uploadCount": 1,
+                  "deletedCount": 0,
+                  "modifyCount": 0,
+                  "movedCount": 0
+              },
+              {
+                  "date": "2024-08-08",
+                  "uploadCount": 0,
+                  "deletedCount": 0,
+                  "modifyCount": 0,
+                  "movedCount": 0
+              },
+              {
+                  "date": "2024-08-09",
+                  "uploadCount": 0,
+                  "deletedCount": 0,
+                  "modifyCount": 0,
+                  "movedCount": 0
+              },
+              {
+                  "date": "2024-08-10",
+                  "uploadCount": 0,
+                  "deletedCount": 0,
+                  "modifyCount": 0,
+                  "movedCount": 0
+              },
+              {
+                  "date": "2024-08-11",
+                  "uploadCount": 0,
+                  "deletedCount": 0,
+                  "modifyCount": 0,
+                  "movedCount": 0
+              },
+              {
+                  "date": "2024-08-12",
+                  "uploadCount": 0,
+                  "deletedCount": 0,
+                  "modifyCount": 0,
+                  "movedCount": 0
+              },
+              {
+                  "date": "2024-08-13",
+                  "uploadCount": 0,
+                  "deletedCount": 0,
+                  "modifyCount": 0,
+                  "movedCount": 0
+              },
+              {
+                  "date": "2024-08-14",
+                  "uploadCount": 0,
+                  "deletedCount": 0,
+                  "modifyCount": 0,
+                  "movedCount": 0
+              }
+          ]
       }
-  ];
+  };
     return response;
   } catch (err) {
     console.error('Error:', err);
@@ -382,4 +391,4 @@ let historyDetailsApi = async (data) => {
   }
 };
 
-export { totalFileInfoApi, fileScanApi, fileVtUploadApi, historyStatisticsApi, historyTrendsApi, historyDetailsApi };
+export { totalFileInfoApi, fileScanApi, fileVtUploadApi, historyTrendsApi, historyDetailsApi };
