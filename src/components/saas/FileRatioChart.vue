3666d9538a62d7d03302985dc18b0ccaa10c8ece
SunM <coex0113@gmail.com>
2024-08-15 20:53:50 +0900

    Feat. DLP 파일 비율 차트 에서 DLP 부분 주석처리

--- src/components/saas/FileRatioChart.vue
+++ src/components/saas/FileRatioChart.vue
@@ -1,17 +1,17 @@
 <template>
   <div class="flex flex-col p-4 bg-white border rounded-lg shadow-sm">
-    <h2 class="text-xl font-bold mb-4">DLP 및 악성 파일 비율</h2>
+    <h2 class="text-xl font-bold mb-4">악성 파일 비율</h2>
     <div class="my-auto">
       <div class="my-0 mx-auto size-64">
         <canvas ref="myChart"></canvas>
       </div>
       <div class="mt-4">
         <ul class="space-y-1.5 mx-5">
-          <li class="flex items-center">
+          <!-- <li class="flex items-center">
             <span class="w-4 h-4 rounded-lg bg-amber-400 mr-2"></span>
             <span class="flex-1 text-sm">민감 파일 비율</span>
             <span class="bg-amber-300 text-slate-50 text-sm text-center w-16 py-0.5 px-2 rounded-xl">{{ dlpRatio }}%</span>
-          </li>
+          </li> -->
           <li class="flex items-center">
             <span class="w-4 h-4 rounded-lg bg-red-600 mr-2"></span>
             <span class="flex-1 text-sm">악성 파일 비율</span>
@@ -42,7 +42,7 @@ const props = defineProps({
 const myChart = ref(null);
 props.fileSize.totalSize = removeZeroDivision(props.fileSize.totalSize);
 const fileVolume = ref(Math.round(props.fileSize.totalSize * 100) / 100);
-const dlpRatio = ref(Math.round((props.fileSize.sensitiveSize / props.fileSize.totalSize) * 100));
+// const dlpRatio = ref(Math.round((props.fileSize.sensitiveSize / props.fileSize.totalSize) * 100));
 const malwareRatio = ref(Math.round((props.fileSize.maliciousSize / props.fileSize.totalSize) * 100));
 
 // watch(() => props.fileSize, (newSize) => {
@@ -51,12 +51,12 @@ const malwareRatio = ref(Math.round((props.fileSize.maliciousSize / props.fileSi
 
 const data = {
   datasets: [
-    {
-      label: 'DLP',
-      data: [dlpRatio.value, 100 - dlpRatio.value],
-      backgroundColor: ['#fbbf24', '#e5e7eb'],
-      hoverOffset: 4
-    },
+    // {
+    //   label: 'DLP',
+    //   data: [dlpRatio.value, 100 - dlpRatio.value],
+    //   backgroundColor: ['#fbbf24', '#e5e7eb'],
+    //   hoverOffset: 4
+    // },
     {
       label: 'Malware',
       data: [malwareRatio.value, 100 - malwareRatio.value],
