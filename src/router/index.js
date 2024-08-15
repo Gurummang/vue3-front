54cc0c3af0f54c7c3a0ba50c96c1999e0eaa4120
SunM <coex0113@gmail.com>
2024-08-15 19:53:34 +0900

    Feat. 라우터 파일 설정(소개딩 버전으로 수정)

--- src/router/index.js
+++ src/router/index.js
@@ -4,7 +4,7 @@ const routes = [
   {
     path: '/',
     name: '메인',
-    component: () => import('../views/MainView.vue')
+    component: () => import('../views/FileDashboardView.vue')
   },
   {
     path: '/login',
@@ -22,11 +22,6 @@ const routes = [
     component: () => import('../views/RegisterSaasView.vue')
   },
   {
-    path: '/register/email',
-    name: '등록 - 이메일알림',
-    component: () => import('../views/RegisterEmailView.vue')
-  },
-  {
     path: '/saas/googledrive',
     name: 'SaaS - Google Drive',
     component: () => import('../views/SaasGoogleDriveView.vue')
@@ -35,11 +30,6 @@ const routes = [
     path: '/saas/slack',
     name: 'SaaS - Slack',
     component: () => import('../views/SaasSlackView.vue')
-  },
-  {
-    path: '/file/dashboard',
-    name: '파일 - 대시보드',
-    component: () => import('../views/FileDashboardView.vue')
   },
   {
     path: '/file/history',
@@ -51,21 +41,6 @@ const routes = [
     name: '파일 - 파일 검사',
     component: () => import('../views/FileScanView.vue')
   },
-  {
-    path: '/users',
-    name: '사용자',
-    component: () => import('../views/UsersView.vue')
-  },
-  {
-    path: '/dlp',
-    name: 'DLP 설정',
-    component: () => import('../views/DlpView.vue')
-  },
-  {
-    path: '/test',
-    name: 'test',
-    component: () => import('../views/testView.vue')
-  }
 ];
 
 const router = createRouter({
