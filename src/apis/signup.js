85f5b47c562643d812bce0fc183f0dae8c7b9e33
SunM <coex0113@gmail.com>
2024-08-16 15:51:28 +0900

    Secu, 로그인 페이지에서의 보안 조치 사항( 특수문자, 예외처리)

--- src/apis/signup.js
+++ src/apis/signup.js
@@ -12,16 +12,16 @@ let gasbLoginApi = async (data) => {
     return false;
   } catch (err) {
     console.error('Error:', err);
-    if (err.response) {
-      // 서버가 응답을 반환한 경우 (예: 400, 401 등의 상태 코드)
-      alert(err.response.data.message || '로그인에 실패했습니다.');
-    } else if (err.request) {
-      // 요청은 보냈지만 응답을 받지 못한 경우
-      alert('서버와의 통신에 실패했습니다. 네트워크 연결을 확인해주세요.');
-    } else {
-      // 요청 설정 중에 문제가 발생한 경우
-      alert('로그인 요청 중 오류가 발생했습니다.');
-    }
+    // if (err.response) {
+    //   // 서버가 응답을 반환한 경우 (예: 400, 401 등의 상태 코드)
+    //   alert(err.response.data.message || '로그인에 실패했습니다.');
+    // } else if (err.request) {
+    //   // 요청은 보냈지만 응답을 받지 못한 경우
+    //   alert('서버와의 통신에 실패했습니다. 네트워크 연결을 확인해주세요.');
+    // } else {
+    //   // 요청 설정 중에 문제가 발생한 경우
+    //   alert('로그인 요청 중 오류가 발생했습니다.');
+    // }
     return false;
   }
 };
