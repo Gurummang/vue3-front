6ff513b7b05820f2b4c222ae53b3aafc11aa582e
SunM <coex0113@gmail.com>
2024-08-19 20:11:10 +0900

    KAN-360 Feat. date 함수 오류 찾기 로그 제거

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -28,7 +28,7 @@ export const convertDate = (date) => {
 }
 
 export const removeWordDate = (date) => {
-  console.log('오류 찾자',date);
+  // console.log('오류 찾자',date);
   if(date == null) return date;
   return date.replace('T', ' ');
 }
