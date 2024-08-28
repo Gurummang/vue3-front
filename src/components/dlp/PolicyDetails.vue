774fc57c27211c6ef4bc351037cddbf6f7bdd4d4
SunM <coex0113@gmail.com>
2024-08-28 19:26:06 +0900

    KAN-419 Feat. 파일 타입 종류 Badge 형태로 보여주기

--- src/components/dlp/PolicyDetails.vue
+++ src/components/dlp/PolicyDetails.vue
@@ -40,9 +40,8 @@
               <svg class="h-5 w-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                 <path fill-rule="evenodd" clip-rule="evenodd" d="M14.795 13.408l5.204 5.204a1 1 0 01-1.414 1.414l-5.204-5.204a7.5 7.5 0 111.414-1.414zM8.5 14A5.5 5.5 0 103 8.5 5.506 5.506 0 008.5 14z" />
               </svg>
-            </button>
-          </div>
-
+              </button>
+            </div>
 
           </div>
         </div>
@@ -72,7 +71,9 @@
                   />
                 </td>
                 <td class="px-2 py-2 whitespace-nowrap text-xs">{{ details.name }}</td>
-                <td class="px-2 py-2 whitespace-nowrap text-xs">{{ details.fileType }}</td>
+                <td class="px-2 py-2 whitespace-nowrap text-xs">
+                  <span v-for="(type, idx) in details.fileType" :key="idx" class="bg-gray-200 text-gray-800 text-xs font-medium me-1 px-1.5 py-0.5 rounded">{{ type }}</span>
+                </td>
                 <td class="px-2 py-2 whitespace-nowrap text-xs">{{ details.description }}</td>
                 <td class="px-2 py-2 whitespace-nowrap text-xs">{{ details.action }}</td>
                 <!-- <td class="px-2 py-2 whitespace-nowrap text-xs text-center">{{ removeWordDate(details.date) }}</td> -->
