db6f05809cf75b5e53ee7b0b96a00905d8e79f82
SunM <coex0113@gmail.com>
2024-08-30 18:24:05 +0900

    KAN-436 Feat. UnconnnectSaas 컴포넌트 구현(2)

--- src/components/UnconnectSaas.vue
+++ src/components/UnconnectSaas.vue
@@ -3,17 +3,24 @@
   <div class="text-center">
     
     <v-icon size="140" color="#FF9A00">mdi-cloud-cancel-outline</v-icon>
-    <h1 class="mt-2 text-xl font-bold tracking-tight text-gray-900">연동부터 해주실래요?</h1>
+    <h1 class="mt-4 text-xl font-bold tracking-tight text-gray-900">{{ $route.name.slice($route.name.lastIndexOf(' ')) }}가 연동되지 않았습니다.</h1>
 
-    <p class="mt-2 text-sm text-gray-900">연동을 좀 해줘봐.</p>
-    <p class="mt-5 text-base font-semibold text-orange-500 italic">새로고침 해보시거나, 문제가 지속적으로 발생하면은 제조사에게 문의해주세요.</p>
+    <!-- <p class="mt-2 text-sm text-gray-900">연동을 좀 해줘봐.</p> -->
+    <button class="mt-6 bg-gradient-to-r from-red-300 to-orange text-white font-bold py-2 px-4 rounded" @click="router.push('/register/saas')">
+        SaaS 연동하기<v-icon :size=20>mdi-arrow-right-thin</v-icon>
+    </button>
+    <!-- <p class="mt-5 text-base font-semibold text-orange-500 italic">새로고침 해보시거나, 문제가 지속적으로 발생하면은 제조사에게 문의해주세요.</p> -->
   </div>
 </div>
 </template>
 
 <script setup>
+import { useRoute, useRouter } from 'vue-router'
+
+const route = useRoute();
+const router = useRouter();
 </script>
 
-<style lang="scss" scoped>
+<style scoped>
 
 </style>
