b96691d548972f7091f146f6b812044211af7e60
SunM <coex0113@gmail.com>
2024-08-30 16:47:31 +0900

    KAN-436 Feat. UnconnectSaas 페이지 임시 구현

---
+++ src/components/UnconnectSaas.vue
@@ -0,0 +1,19 @@
+<template>
+<div class="h-[50vh] place-content-center bg-white p-4 rounded-lg shadow-sm">
+  <div class="text-center">
+    
+    <v-icon size="140" color="#FF9A00">mdi-cloud-cancel-outline</v-icon>
+    <h1 class="mt-2 text-xl font-bold tracking-tight text-gray-900">연동부터 해주실래요?</h1>
+
+    <p class="mt-2 text-sm text-gray-900">연동을 좀 해줘봐.</p>
+    <p class="mt-5 text-base font-semibold text-orange-500 italic">새로고침 해보시거나, 문제가 지속적으로 발생하면은 제조사에게 문의해주세요.</p>
+  </div>
+</div>
+</template>
+
+<script setup>
+</script>
+
+<style lang="scss" scoped>
+
+</style>
