be665c732a40f7fd389b33ddf2eb623c50201f5a
SunM <coex0113@gmail.com>
2024-09-08 11:28:40 +0900

    KAN-421 Feat. 정책 추가 페이지에서 input 태그

--- src/components/dlp/PolicyAdd.vue
+++ src/components/dlp/PolicyAdd.vue
@@ -2,8 +2,52 @@
   <div>
     <div class="bg-white shadow-sm rounded-lg p-4 mb-5">
       <div class="mb-2">
-        <h2 class="text-xl font-bold text-gray-800 mb-4">DLP 정책 목록</h2>
-      
+        <h2 class="text-xl font-bold text-gray-800 mb-4">DLP 정책 추가</h2>
+
+        <div class="mb-2">
+          <label for="SaasAlias" class="block text-sm font-semibold text-gray-700"> 정책명 </label>
+          <input
+            type="text"
+            id="SaasAlias"
+            placeholder="연동할 SaaS의 별칭을 입력해주세요."
+            class="mt-1 p-1.5 w-full rounded-md shadow-sm text-xs border-2 border-gray-300"
+            v-model="alias"
+          />
+        </div>
+
+        <div class="mb-2">
+          <label for="SaasAlias" class="block text-sm font-semibold text-gray-700"> 정책 설명 </label>
+          <input
+            type="text"
+            id="SaasAlias"
+            placeholder="연동할 SaaS의 별칭을 입력해주세요."
+            class="mt-1 p-1.5 w-full rounded-md shadow-sm text-xs border-2 border-gray-300"
+            v-model="alias"
+          />
+        </div>
+
+        <!-- <div class="mb-2">
+          <label class="inline-flex items-center">
+            <input type="checkbox" v-model="agreeToTerms" class="form-checkbox size-4" />
+            <span class="ml-2 text-sm">위 SaaS 연동을 동의하겠습니다.</span>
+          </label>
+        </div> -->
+
+        <div class="mb-2">
+          <label for="SaasAlias" class="block text-sm font-semibold text-gray-700"> 권장 조치 </label>
+          <input
+            type="text"
+            id="SaasAlias"
+            placeholder="연동할 SaaS의 별칭을 입력해주세요."
+            class="mt-1 p-1.5 w-full rounded-md shadow-sm text-xs border-2 border-gray-300"
+            v-model="alias"
+          />
+        </div>
+
+      </div>
+      <div class="flex justify-end py-3 gap-2">
+        <button @click="syncSaaS" class="bg-white text-orange-500 px-4 py-2 rounded text-sm font-semibold hover:bg-orange-600">정책 취소</button>
+        <button @click="syncSaaS" class="bg-orange-500 text-white px-4 py-2 rounded text-sm font-semibold hover:bg-orange-600">정책 추가</button>
       </div>
     </div>
 
