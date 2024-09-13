900c3e6f7957fa0ae96464119a93ca4fa89132af
SunM <coex0113@gmail.com>
2024-09-13 17:55:46 +0900

    KAN-466 Feat. 다중 이메일 수신 및 알림 Toggle 기능 구현

--- src/components/modals/EmailCreatModal.vue
+++ src/components/modals/EmailCreatModal.vue
@@ -1,31 +1,14 @@
 <template>
   <div class="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
-    <div class="bg-white rounded-lg shadow-lg w-1/4 max-w-lg">
+    <div class="bg-white rounded-lg shadow-lg w-1/2">
       <div class="flex justify-end p-3">
         <button @click="$emit('close')" class="text-gray-400 hover:text-black">
           <v-icon>mdi-close</v-icon>
         </button>
       </div>
-      <div class="flex justify-center p-3">
-        <img src="@/assets/grummang_mascot_small.png" alt="구름망 캐릭터" class="size-1/3 object-cover rounded-full">
-      </div>
       <div class="px-4">
         <div class="mb-2">
-          <label for="saasType" class="block text-sm font-semibold text-gray-700"> SaaS 종류 </label>
-          <select name="SaasType"
-            id="SaasType" 
-            size="1" 
-            class="mt-1 p-1.5 w-full rounded-md shadow-sm text-xs border-2 border-gray-300"
-            v-model="saasType"
-          >
-            <option value="None" selected disabled hidden>연동할 SaaS를 선택해주세요.</option>
-            <option value="6">Google Drive</option>
-            <option value="1">Slack</option>
-            <!-- <option value="3">O365</option> -->
-          </select>
-        </div>
-        <div class="mb-2">
-          <label for="SaasAlias" class="block text-sm font-semibold text-gray-700"> 연동 별칭 </label>
+          <label for="SaasAlias" class="block text-sm font-semibold text-gray-700"> 이메일 제목 </label>
           <input
             type="text"
             id="SaasAlias"
@@ -35,71 +18,67 @@
           />
         </div>
         <div class="mb-2">
-          <label for="SaaSEmail" class="block text-sm font-semibold text-gray-700"> SaaS 관리자 이메일 </label>
-          <input
-            type="email"
-            required
-            id="SaaSEmail"
-            placeholder="SaaS의 관리자 계정 이메일을 입력해주세요."
-            class="mt-1 p-1.5 w-full rounded-md shadow-sm text-xs border-2 border-gray-300"
-            v-model="saasEmail"
-          />
-          <p v-if="!isValidEmail" class="text-rose-500">이메일 형식을 맞춰서 작성해주세요.</p>
-        </div>
-        <div v-if="showApiInput" class="mb-2 relative">
-          <label for="ApiKey" class="block text-sm font-semibold text-gray-700">API Key</label>
-          <div class="relative">
-            <form>
-              <input
-                :type="showPassword ? 'password' : 'text'"
-                id="ApiKey"
-                v-model="apiToken"
-                placeholder="SaaS의 API Key를 입력해주세요."
-                class="mt-1 p-1.5 w-full pr-10 rounded-md shadow-sm text-xs border-2 border-gray-300"
-                autocomplete="on"
-              />
-              <button 
-                @click="togglePasswordVisibility" 
-                type="button"
-                :class="['absolute right-0 pr-2 flex items-center', isValidApiToken ? 'top-5 bottom-5' : '-top-5 -bottom-0']"
-              >
-                <v-icon>{{ showPassword ? 'mdi-eye-closed' : 'mdi-eye' }}</v-icon>
-              </button>
-            </form>
-            <p v-if="!isValidApiToken" class="text-rose-500">해당 API Token이 올바르지 않습니다.</p>
+          <label for="SaaSEmail" class="block text-sm font-semibold text-gray-700"> 이메일 수신 계정 </label>
+          <div class="mt-1 flex flex-wrap items-center border-2 border-gray-300 rounded-md focus-within:border-black">
+            <template v-for="(email, index) in emails" :key="index">
+              <span class="flex items-center m-0.5 px-2 py-1 bg-orange text-white rounded-full text-xs">
+                {{ email }}
+              <v-icon :size="12" @click="removeEmail(index)" class="ml-1.5 hover:text-orange-900">mdi-close</v-icon>
+              </span>
+            </template>
+            <input
+              v-model="newEmail"
+              type="email"
+              @keydown.enter.prevent="addEmail"
+              placeholder="이메일 주소 입력 후 엔터를 눌러주세요."
+              class="flex-grow p-1.5 text-xs rounded-md focus:outline-none"
+              ref="emailInput"
+            >
           </div>
-        </div>  
-        <div class="mb-2">
-          <label for="WebhookUrl" class="block text-sm font-semibold text-gray-700"> Webhhook URL </label>
-          <input
-            type="text"
-            id="WebhookUrl"
-            placeholder="위 SaaS는 Webhook URL을 지원하지 않습니다."
-            class="mt-1 p-1.5 w-full rounded-md shadow-sm text-xs border-2 border-gray-300"
-            v-model="webhookUrl"
-            readonly
-          />
-        </div>
-        <div class="mb-2">
-          <label class="inline-flex items-center">
-            <input type="checkbox" v-model="agreeToTerms" class="form-checkbox size-4" />
-            <span class="ml-2 text-sm">위 SaaS 연동을 동의하겠습니다.</span>
+          <p v-if="errorEmail" class="mt-1 text-sm text--rose-500">{{ errorEmail }}</p>
+        </div> 
+        <div class="mb-5">
+          <label for="SaaSEmail" class="block text-sm font-semibold text-gray-700"> 이메일 내용 </label>
+          <textarea rows="10" class="mt-1 p-1.5 w-full rounded-md shadow-sm text-xs border-2 border-gray-300"></textarea>
+        </div> 
+        <!-- Items Toggle -->
+        <div class="flex justify-between mb-5 pb-2 w-60 border-b-2">
+          <span class="text-sm font-semibold text-gray-700">악성코드 탐지</span>
+          <label class="inline-flex items-center cursor-pointer">
+            <input type="checkbox" value="" class="sr-only peer">
+            <div class="relative w-9 h-5 bg-gray-200 rounded-full peer peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-4 after:w-4 after:transition-all peer-checked:bg-orange"></div>
           </label>
-        </div>
+        </div> 
+        <div class="flex justify-between mb-5 pb-2 w-60 border-b-2">
+          <span class="text-sm font-semibold text-gray-700">DLP 탐지</span>
+          <label class="inline-flex items-center cursor-pointer">
+            <input type="checkbox" value="" class="sr-only peer">
+            <div class="relative w-9 h-5 bg-gray-200 rounded-full peer peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-4 after:w-4 after:transition-all peer-checked:bg-orange"></div>
+          </label>
+        </div> 
+        <div class="flex justify-between mb-2 pb-2 w-60 border-b-2">
+          <span class="text-sm font-semibold text-gray-700">VirusTotal 탐지</span>
+          <label class="inline-flex items-center cursor-pointer">
+            <input type="checkbox" value="" class="sr-only peer">
+            <div class="relative w-9 h-5 bg-gray-200 rounded-full peer peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-4 after:w-4 after:transition-all peer-checked:bg-orange"></div>
+          </label>
+        </div> 
       </div>
+
+
       <div class="flex justify-end p-3">
         <button v-if="saasType != 6" @click="syncSaaS" class="bg-orange-500 text-white px-4 py-2 rounded text-sm font-semibold hover:bg-orange-600">SaaS 연동하기</button>
         <button v-else @click="googleOAuth2" class="bg-orange-500 text-white px-4 py-2 rounded text-sm font-semibold hover:bg-orange-600">SaaS 연동하기</button>
       </div>
     </div>
   </div>
+<!-- <saas-error-modal
+  v-if="isErrorModalOpen"
+  :errorCode="errorCode"
+  :errorType="'연동'"
+  @close="closeErrorModal"
+></saas-error-modal> -->
 
-  <saas-error-modal
-    v-if="isErrorModalOpen"
-    :errorCode="errorCode"
-    :errorType="'연동'"
-    @close="closeErrorModal"
-  ></saas-error-modal>
 </template>
 
 <script setup>
@@ -107,7 +86,7 @@ import { ref, defineProps, defineEmits, watch } from 'vue';
 import axios from 'axios';
 import saasErrorModal from '@/components/modals/SaasErrorModal.vue'
 import { validateEmail } from '@/utils/validation.js'
-import { getWebhookApi, TokenValidationApi, connectSaasApi } from '@/apis/register.js'
+// import { getWebhookApi, TokenValidationApi, connectSaasApi } from '@/apis/register.js'
 import { htmlEscape, specialChar } from '@/utils/security.js';
 
 let emit = defineEmits(['close']);
@@ -121,7 +100,7 @@ let apiToken = ref('');
 let agreeToTerms = ref(false);
 
 let showPassword = ref(true);
-let isValidEmail = ref(true);
+// let isValidEmail = ref(true);
 let showApiInput = ref(true);
 let isValidApiToken = ref(true);
 let selectedSaaS = ref(null);
@@ -136,184 +115,46 @@ const closeErrorModal = () => {
   isErrorModalOpen.value = false;
 }
 
-const syncSaaS = () => {
-  // 보안 조치
-  const safeAlias = ref(htmlEscape(alias.value));
-  const safeSaasEmail = ref(htmlEscape(saasEmail.value));
-  const safeApiToken = ref(htmlEscape(apiToken.value));
+// 구분선
 
-  if(!saasType.value || saasType.value === 'None') {
-    alert('연동할 SaaS가 정의되지 않았습니다.');
-    return;
-  }
-  if(!SaasAlias.value) {
-    alert('연동 별칭이 정의되지 않았습니다.\n해당 칸에 작성해주세요.');
-    return;
-  } else if(specialChar(safeAlias.value)) {
-    alert('연동 별칭에 특수 문자가 포함되어 있습니다. 다시 작성해주세요.');
-    return;
-  }
-  if(!SaaSEmail.value) {
-    alert('SaaS 관리자 이메일이 정의되지 않았습니다.\n해당 칸에 다시 작성해주세요.');
-    return;
-  } else if(specialChar(safeSaasEmail.value)) {
-    alert('관리자 이메일에 특수 문자가 포함되어 있습니다. 다시 작성해주세요.');
-    return;
-  }
-  else if(!isValidEmail.value) {
-    alert('이메일 형식이 올바르지 않습니다.\n다시 작성해주세요.');
-    return;
-  }
-  if(!apiToken.value) {
-    alert('SaaS의 API Key 값이 정의되지 않았습니다.\n해당 칸에 작성해주세요.');
-    return;
-  } else if(specialChar(safeApiToken.value)) {
-    alert('API Key 값에 특수 문자가 포함되어 있습니다. 다시 작성해주세요.');
-    return;
-  }
-  else if(!isValidApiToken.value) {
-    alert('해당 API Key이 올바르지 않습니다.\n다시 작성해주세요.');
-    return;
-  }
-  if (!agreeToTerms.value) {
-    alert('SaaS 연동을 위해 체크박스로 연동에 동의 해야합니다.');
-    return;
-  }
+const emails = ref([])
+const newEmail = ref('')
+const errorEmail = ref('')
+const emailInput = ref(null)
 
-  // 다음 스텝 -> 해당 값들을 POST로 보내기
-  let connectData = {
-    "orgId": 1,     // samsung
-    "saasId": saasType.value,    // slack
-    "alias": safeAlias.value,
-    "adminEmail": safeSaasEmail.value,
-    "apiToken": safeApiToken.value,
-    "webhookUrl": webhookUrl.value
-  };
+// const isValidEmail = (email) => {
+//   const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+//   return re.test(email)
+// }
 
-  connectSaasApi(connectData).then((response) => {
-    console.log(response);
-    errorCode = response.errorCode;
-    if(errorCode != 200) {
-      openErrorModal();
-      watch(isErrorModalOpen, (afterValue, beforeValue) => {
-        if (afterValue === false) {
-          emit('close');
-        }
-      });
+const addEmail = () => {
+  const email = newEmail.value.trim()
+  if (email && validateEmail(email)) {
+    if (!emails.value.includes(email)) {
+      emails.value.push(email)
+      newEmail.value = ''
+      errorEmail.value = ''
+    } else {
+      errorEmail.value = '이미 등록된 이메일입니다.'
     }
-    else {
-      emit('close');
-    }
-  })
-  .catch(err => alert(err + "\n서버에 문제가 발생했어요."));
-};
-
-const googleOAuth2 = () => {
-  // 보안 조치
-  const safeAlias = ref(htmlEscape(alias.value));
-  const safeSaasEmail = ref(htmlEscape(saasEmail.value));
-
-  if(!saasType.value || saasType.value === 'None') {
-    alert('연동할 SaaS가 정의되지 않았습니다.');
-    return;
-  }
-  if(!SaasAlias.value) {
-    alert('연동 별칭이 정의되지 않았습니다.\n해당 칸에 작성해주세요.');
-    return;
-  } else if(specialChar(safeAlias.value)) {
-    alert('연동 별칭에 특수 문자가 포함되어 있습니다. 다시 작성해주세요.');
-    return;
-  }
-  if(!SaaSEmail.value) {
-    alert('SaaS 관리자 이메일이 정의되지 않았습니다.\n해당 칸에 다시 작성해주세요.');
-    return;
-  } else if(specialChar(safeSaasEmail.value)) {
-    alert('관리자 이메일에 특수 문자가 포함되어 있습니다. 다시 작성해주세요.');
-    return;
-  }
-  if (!agreeToTerms.value) {
-    alert('SaaS 연동을 위해 체크박스로 연동에 동의 해야합니다.');
-    return;
-  }
-  
-  const clientId = import.meta.env.VITE_GOOGLEDRIVE_CLIENTID;
-  const responseType = import.meta.env.VITE_GOOGLEDRIVE_RESPONSETYPE;
-  const redirectUri = import.meta.env.VITE_GOOGLEDRIVE_REDIRECTURL;
-  const scope = import.meta.env.VITE_GOOGLEDRIVE_SCOPE;
-  
-  const authUrl = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${clientId}&redirect_uri=${redirectUri}&response_type=${responseType}&scope=${scope}`;
-  // window.location.href = authUrl;
-  // 새 탭에서 인증 페이지 열기
-  const authWindow = window.open(authUrl, '_blank');
-  // 새 창에서 인증 페이지 열기
-  // const authWindow = window.open(authUrl, '_blank', 'width=500,height=600');
-
-  // 선택적: 팝업이 차단되었는지 확인
-  if (authWindow === null || typeof(authWindow) === 'undefined') {
-    alert('팝업이 차단되었습니다. 팝업을 허용해주세요.');
-    return;
-  }
-
-  let connectData = {
-    "orgId": 1,     // samsung
-    "saasId": saasType.value,
-    "alias": safeAlias.value,
-    "adminEmail": safeSaasEmail.value,
-  };
-
-  connectSaasApi(connectData).then((response) => {
-    console.log(response);
-    errorCode = response.errorCode;
-    if(errorCode != 200) {
-      openErrorModal();
-      watch(isErrorModalOpen, (afterValue, beforeValue) => {
-        if (afterValue === false) {
-          emit('close');
-        }
-      });
-    }
-    else {
-      emit('close');
-    }
-  })
-  .catch(err => alert(err + "\n서버에 문제가 발생했어요."));
-}
-
-
-const togglePasswordVisibility = () => {
-  showPassword.value = !showPassword.value;
-};
-
-const validateAdminEmail = () => {
-  isValidEmail.value = validateEmail(saasEmail.value);
-};
-
-const validateWebhook = () => {
-  if(saasType.value != 'None') {
-    getWebhookApi(saasType.value).then((response) => {
-      webhookUrl.value = response;
-    });
-  }
-  if(saasType.value != '6') {
-    showApiInput.value = true;
-  }
-  else {
-    showApiInput.value = false;
+  } else if (email) {
+    errorEmail.value = '유효하지 않은 이메일 주소입니다.'
   }
 }
 
-const validateApiToken = () => {
-  let data = {
-    "apiToken": apiToken.value
-  }
-  TokenValidationApi(data, 1).then((response) => {
-    isValidApiToken.value = response;
-  });
-  console.log('API 토큰 검증이래:  '+ isValidApiToken.value);
+const removeEmail = (index) => {
+  emails.value.splice(index, 1)
 }
 
-watch(saasEmail, validateAdminEmail);
-watch(apiToken, validateApiToken);
-watch(saasType, validateWebhook);
+// const handleBackspace = (e) => {
+//   if (newEmail.value === '' && emails.value.length > 0) {
+//     emails.value.pop()
+//   }
+// }
+
+watch(emails, (newEmails) => {
+  console.log('등록된 이메일:', newEmails)
+  console.log(emails.value);
+})
 
 </script>
