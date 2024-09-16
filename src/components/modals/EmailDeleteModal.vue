f8a0a47ea79ace23c731d65ea763ca76dc73a2f4
SunM <coex0113@gmail.com>
2024-09-16 18:30:47 +0900

    KAN-468 Design. 이메일 알림 삭제 모달창 디자인 구현

--- src/components/modals/EmailDeleteModal.vue
+++ src/components/modals/EmailDeleteModal.vue
@@ -1,43 +1,40 @@
 <template>
-  <div class="fixed inset-0 bg-gray-500 bg-opacity-75 flex items-center justify-center">
-    <div class="bg-white rounded-lg shadow-xl w-1/4">
-      <div class="p-3">
-        <div class="flex justify-end">
-          <button @click="$emit('close')" class="text-gray-400 hover:text-black">
-            <v-icon>mdi-close</v-icon>
-          </button>
+  <div class="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
+    <div class="bg-white rounded-lg shadow-lg w-1/4 max-w-lg">
+      <div class="flex justify-end p-3">
+        <button @click="$emit('close')" class="text-gray-400 hover:text-black">
+          <v-icon>mdi-close</v-icon>
+        </button>
+      </div>
+      <div class="px-4">
+        <div class="mb-2 space-y-2 text-center">
+          <label for="saasType" class="block pb-2 text-2xl font-semibold text-red-600"> 
+            <v-icon :size="30">mdi-email-remove-outline</v-icon> 이메일 알림 삭제 </label>
+          <p class="text-base pb-1 font-semibold">"{{ props.selectedEmail.title }}"</p>
+          <p class="text-base pb-3">위 이메일 알림을 정말 삭제하겠습니까?</p>
+          <p class="text-xs text-red-600 font-semibold">해당 이메일 알림을 삭제하면은 <br>앞으로 탐지 알림을 발송하지 않습니다.</p>
         </div>
+      </div>
 
-        <p class="text-sm text-red-700 font-bold text-center mb-2">위 SaaS의 연동 해제하시겠습니까?</p>
-        <p class="text-xs text-black text-center mb-2">
-          위 SaaS의 연동 해제하면은<br> 앞으로 CASB에서 관리될 수 없습니다.<br>
-          정말 Jira의 연동을 해제하시겠습니까?
-        </p>
+      <div class="px-3 pt-3">
+        <label class="inline-flex items-center">
+          <input type="checkbox" v-model="confirmed" class="form-checkbox size-3.5" />
+          <span class="ml-2 text-xs font-semibold">위 이메일 알림을 삭제하는 것에 동의하겠습니다.</span>
+        </label>
+      </div>
 
-        <div class="py-4">
-          <label class="inline-flex items-center">
-            <input type="checkbox" v-model="confirmed" class="form-checkbox size-4" />
-            <span class="ml-2 text-sm font-bold">위 SaaS 연동을 해제하는 것에 동의하겠습니다.</span>
-          </label>
-        </div>
-
-            <!-- class="px-4 py-2 border border-gray-300 rounded-md text-gray-700 hover:bg-gray-50" -->
-        <div class="grid grid-cols-2 gap-4">
-          <button 
-            class="inline-block rounded border border-indigo-900 px-4 py-2 align-text-bottom text-sm font-semibold text-indigo-900 hover:bg-indigo-900 hover:text-white active:bg-indigo-900"
-            @click="$emit('close')"
-          >
-            아니요
-          </button>
-            <!-- :class="['inline-block rounded px-6 py-3 align-text-bottom text-base font-bold text-rose-600', confirmed ? 'hover: border border-rose-600 bg-rose-600 hover:text-white active:bg-rose-600' : 'bg-gray-400 cursor-not-allowed']" -->
-          <button
-            :class="['inline-block px-4 py-2 rounded align-text-bottom text-white text-sm font-semibold', confirmed ? 'bg-red-600 hover:bg-red-700' : 'bg-gray-400 cursor-not-allowed']"
-            :disabled="!confirmed"
-            @click="UnconnectSaas(saasId)"
-          >
-            예
-          </button>
-        </div>
+      <div class="flex justify-center p-3 space-x-2">
+        <button  
+          @click="$emit('close')" 
+          class="w-1/2 border border-red-600 px-3 py-2 align-text-bottom text-sm font-semibold text-red-600 hover:bg-red-800 hover:text-white active:bg-red-800"
+        >아니오
+        </button>
+        <button 
+          @click="$emit('close')" 
+          :disabled="!confirmed"
+          :class="['w-1/2 border px-3 py-2 align-text-bottom text-sm font-semibold text-white', confirmed ? 'bg-red-600 border-red-600 hover:bg-red-800 active:bg-red-800' : 'bg-gray-400 border-gray-400 cursor-not-allowed']"
+        >예
+        </button>
       </div>
     </div>
   </div>
@@ -59,16 +56,18 @@ import { unconnectSaasApi } from '@/apis/register.js'
 const props = defineProps({
   selectedEmail: {
     type: Object,
-    // required: true
+    required: true
   }
 });
 
 let emit = defineEmits(['close'])
 
-let saasId = ref(props.selectedEmail);
+let emailId = ref(props.selectedEmail.id);
 let confirmed = ref(false);
 let isErrorModalOpen = ref(false);
 let errorCode = ref(null);
+
+console.log(emailId.value);
 
 const openErrorModal = () => {
   isErrorModalOpen.value = true;
@@ -76,29 +75,6 @@ const openErrorModal = () => {
 
 const closeErrorModal = () => {
   isErrorModalOpen.value = false;
-}
-
-const UnconnectSaas = (saasId) => {
-  console.log(saasId);
-
-  let deleteInfo = {
-    "id": saasId
-  }
-  unconnectSaasApi(deleteInfo).then((response) => {
-    errorCode.value = response.errorCode;
-    console.log(response);
-    if(errorCode.value != 200) {
-      openErrorModal();
-      watch(isErrorModalOpen, (afterValue, beforeValue) => {
-        if (afterValue === false) {
-          emit('close');
-        }
-      });
-    }
-    else {
-      emit('close');
-    }
-  }).catch(err => alert(err + "\n서버에 문제가 발생했어요."));
 }
 
 </script>
