47522acfad9f68dd8a8988da03de33c81384cc5e
SunM <coex0113@gmail.com>
2024-09-21 11:10:15 +0900

    KAN-447 Feat. Dlp cl제거 및 예시값 코드 제거

--- src/apis/dlp.js
+++ src/apis/dlp.js
@@ -14,11 +14,10 @@ let dlpPolicyListApi = async () => {
       return;
     }
     if(response.status == '200') {
-      // console.log(response.data);
       return response.data;
     }
   } catch (err) {
-    console.error('Error:', err);
+    // console.error('Error:', err);
     throw err;  
   }
 };
@@ -31,28 +30,26 @@ let dlpPolicyMakeApi = async (data) => {
       return;
     }
     if(response.status == '200') {
-      console.log(response.data);
       return response.data;
     }
   } catch (err) {
-    console.error('Error:', err);
+    // console.error('Error:', err);
     throw err;  
   }
 };
 
 let dlpPolicyDeleteApi = async (data) => {
   try {
-    const response = await api.post('/api/v1/vt/policy/delete', data);
+    const response = await api.post('/api/v1/dlp/policy/delete', data);
     if(response.data[0].success == false) {
       router.push('/login');
       return;
     }
     if(response.status == '200') {
-      console.log(response.data);
       return response.data;
     }
   } catch (err) {
-    console.error('Error:', err);
+    // console.error('Error:', err);
     throw err;  
   }
 };
