6746177dcf4e15278774c07094b3d96e294fb851
SunM <coex0113@gmail.com>
2024-09-28 01:04:34 +0900

    KAN-524 Feat. 파일검사 - 페이지 이동 시 accordion들 초기화

--- src/components/file/FileDetails.vue
+++ src/components/file/FileDetails.vue
@@ -364,6 +364,11 @@ const props = defineProps({
 
 const sortedDate = ref(props.fileDetails.data.files.sort((a, b) => new Date(b.date) - new Date(a.date)))
 
+const accordionStatus = ref({})
+const gscanStatus = ref({})
+const dlpReportStatus = ref({})
+const virusTotalReportStatus = ref({})
+
 // 페이지 네비게이션
 const items = ref([])
 const totalData = ref([])
@@ -402,6 +407,11 @@ const reset = (pageIdx) => {
 
 watch(selectPages, () => {
   getData()
+  clearCheckedIndex()
+  accordionStatus.value = {}
+  gscanStatus.value = {}
+  dlpReportStatus.value = {}
+  virusTotalReportStatus.value = {}
 })
 
 let checkedIndex = ref([])
@@ -409,11 +419,6 @@ let checkedIndex = ref([])
 const clearCheckedIndex = () => {
   checkedIndex.value = []
 }
-
-const accordionStatus = ref({})
-const gscanStatus = ref({})
-const dlpReportStatus = ref({})
-const virusTotalReportStatus = ref({})
 
 const isVirustotalModalOpen = ref(false)
 const isFileDeleteModalOpen = ref(false)
