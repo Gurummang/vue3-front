e587c301dbab1c0716c83c8a87de8e35d6a316bb
SunM <coex0113@gmail.com>
2024-10-03 01:22:02 +0900

    KAN-534 Feat. 1차적인 라인 차트 구현

---
+++ src/components/main/FileHistoryLine.vue
@@ -0,0 +1,98 @@
+<template>
+  <div class="bg-white p-4 rounded-lg shadow-sm mb-5">
+    <h2 class="text-xl font-bold mb-4">히스토리 추이</h2>
+    <canvas ref="chartRef" class=""></canvas>
+    <div class="flex justify-end items-center">
+      <span class="size-3 rounded bg-orange"></span>
+      <span class="text-xs px-1.5">업로드</span>
+      <span class="size-3 rounded bg-amber-400"></span>
+      <span class="text-xs px-1.5">수정</span>
+      <span class="size-3 rounded bg-slate-400"></span>
+      <span class="text-xs px-1.5">삭제</span>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue';
+import { Chart, LineController, LineElement,PointElement, LinearScale, Title,CategoryScale,RadialLinearScale, ArcElement, Tooltip, Legend } from 'chart.js';
+
+Chart.register(LineController, LineElement, PointElement, LinearScale, Title, CategoryScale, RadialLinearScale, ArcElement, Tooltip, Legend);
+
+const props = defineProps({
+  fileHistoryLine: {
+    type: Object,
+    required: true
+  }
+});
+
+const chartRef = ref(null);
+
+const chartData = ref(props.fileHistoryLine)
+chartData.value.sort((a, b) => {
+  const dateA = new Date(`${a.month}-01`)
+  const dateB = new Date(`${b.month}-01`)
+  return dateA - dateB
+})
+
+onMounted(() => {
+  const ctx = chartRef.value.getContext('2d');
+  
+  new Chart(ctx, {
+    type: 'line',
+    data: {
+      labels: chartData.value.map(row => row.month),
+      datasets: [{
+        label: '히스토리',
+        data: chartData.value.map(row => row.total),
+        fill: false,
+        borderColor: '#4D4C7D',
+        backgroundColor: '#4D4C7D',
+        // borderDash: [4, 3],
+        // hoverBorderDash: [0, 0],
+        pointRadius: 3,
+        pointHoverRadius: 3,
+        borderWidth: 2,
+        hoverBorderWidth: 2,
+        tension: 0.3,
+      }]
+    },
+    options: {
+      responsive: true,
+      aspectRatio: 3,
+      hover: {
+        mode: 'dataset',
+        intersect: false
+      },
+      plugins: {
+        legend: {
+          display: false,
+          position: 'bottom',
+        },
+        tooltip: {
+          mode: 'nearest',
+          intersect: true,
+        }
+      },
+      scales: {
+        y: {
+          beginAtZero: true,
+          title: {
+            display: false,
+            text: '' 
+          },
+          ticks: {
+            stepSize: 10
+          }
+        },
+        x: {
+          title: {
+            display: false,
+            text: '달'
+          },
+        }
+      }
+    }
+  });
+});
+</script>
