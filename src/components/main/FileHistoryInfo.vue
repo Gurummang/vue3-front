85c9c3ac9d97aca02d3cc34a4bdd1a75513ffd93
SunM <coex0113@gmail.com>
2024-10-03 02:14:43 +0900

    KAN-534 Feat. HistoryInfo 컴포넌트 오른쪽 높이 맞추기

--- src/components/main/FileHistoryInfo.vue
+++ src/components/main/FileHistoryInfo.vue
@@ -1,47 +1,43 @@
 <template>
-  <div>
-    <div class="h-[20rem] bg-white shadow-sm rounded-lg p-4 mb-5">
-      <div class="mb-2">
-        <h2 class="text-xl font-bold mb-4">SaaS별 히스토리</h2>
-      </div>
-      <div class="overflow-x-auto">
-        <table class="min-w-full bg-white">
-          <thead class="bg-indigo-900">
-            <tr>
-              <th class="px-2 py-4 w-[30%] text-left text-sm font-bold font-medium text-white tracking-wider">SaaS</th>
-              <th class="px-1 py-4 w-[15%] text-center text-sm font-bold font-medium text-white tracking-wider">업로드</th>
-              <th class="px-1 py-4 w-[15%] text-center text-sm font-bold font-medium text-white tracking-wider">수정</th>
-              <th class="px-1 py-4 w-[15%] text-center text-sm font-bold font-medium text-white tracking-wider">삭제</th>
-              <th class="px-1 py-4 w-[25%] text-left text-sm font-bold font-medium text-white tracking-wider">마지막 활동</th>
+  <div class="h-hull bg-white shadow-sm rounded-lg p-4 mb-5">
+    <div class="mb-2">
+      <h2 class="text-xl font-bold mb-4">SaaS별 히스토리</h2>
+    </div>
+    <div class="overflow-x-auto">
+      <table class="min-w-full bg-white">
+        <thead class="bg-indigo-900">
+          <tr>
+            <th class="px-2 py-4 w-[30%] text-left text-sm font-bold font-medium text-white tracking-wider">SaaS</th>
+            <th class="px-1 py-4 w-[15%] text-center text-sm font-bold font-medium text-white tracking-wider">업로드</th>
+            <th class="px-1 py-4 w-[15%] text-center text-sm font-bold font-medium text-white tracking-wider">수정</th>
+            <th class="px-1 py-4 w-[15%] text-center text-sm font-bold font-medium text-white tracking-wider">삭제</th>
+            <th class="px-1 py-4 w-[25%] text-left text-sm font-bold font-medium text-white tracking-wider">마지막 활동</th>
+          </tr>
+        </thead>
+        <tbody class="bg-white divide-y divide-gray-200">
+          <template v-for="(info, index) in InfoValues" :key="index">
+            <tr class="hover:bg-gray-100 cursor-pointer">
+              <td class="flex items-center justify-center px-2 py-4">
+                <img class="flex-shrink-0 size-5 rounded-full mr-2" :src="getSaasImg(convertSaasName(info.saas))" :alt="info.saas" />
+                <div class="w-full text-sm font-semibold truncate capitalize">{{ convertSaasName(info.saas) }}</div>
+              </td>
+              <td class="px-1 py-4">
+                <div class="w-full text-sm text-center">{{ info.upload }}</div>
+              </td>
+              <td class="px-1 py-4">
+                <div class="w-full text-sm text-center">{{ info.change }}</div>
+              </td>
+              <td class="px-1 py-4">
+                <div class="w-full text-sm text-center">{{ info.delete }}</div>
+              </td>
+              <td class="px-1 py-4">
+                <div class="w-full text-sm text-left">{{ info.lastActivity }}</div>
+              </td>
             </tr>
-          </thead>
-          <tbody class="bg-white divide-y divide-gray-200">
-            <template v-for="(info, index) in InfoValues" :key="index">
-              <tr class="hover:bg-gray-100 cursor-pointer">
-                <td class="flex items-center justify-center px-2 py-4">
-                  <img class="flex-shrink-0 size-5 rounded-full mr-2" :src="getSaasImg(convertSaasName(info.saas))" :alt="info.saas" />
-                  <div class="w-full text-sm font-semibold truncate capitalize">{{ convertSaasName(info.saas) }}</div>
-                </td>
-                <td class="px-1 py-4">
-                  <div class="w-full text-sm text-center">{{ info.upload }}</div>
-                </td>
-                <td class="px-1 py-4">
-                  <div class="w-full text-sm text-center">{{ info.change }}</div>
-                </td>
-                <td class="px-1 py-4">
-                  <div class="w-full text-sm text-center">{{ info.delete }}</div>
-                </td>
-                <td class="px-1 py-4">
-                  <div class="w-full text-sm text-left">{{ info.lastActivity }}</div>
-                </td>
-                
-                
-              </tr>
-            </template>
-              <!-- Accordion row -->
-          </tbody>
-        </table>
-      </div>
+          </template>
+            <!-- Accordion row -->
+        </tbody>
+      </table>
     </div>
   </div>
 </template>
