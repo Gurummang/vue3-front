be84f52f239c1e245214afaafc2228bef5ed7376
SunM <coex0113@gmail.com>
2024-10-02 17:10:58 +0900

    KAN-533 Feat. cors문제로 response. 값 할당하여 return

--- src/apis/main.js
+++ src/apis/main.js
@@ -8,14 +8,64 @@ const api = axios.create({
 
 let mainTotalApi = async () => {
   try {
-    const response = await api.get('/api/v1/main/total');
-    if(response.data.status == 'error') {
-      router.push('/');
-      return;
+    const response = {
+      "status": "success",
+      "data": {
+          "saas": [
+              {
+                  "saas": "slack",
+                  "alias": "fffffff",
+                  "email": "sasd@123123.vv"
+              },
+              {
+                  "saas": "o365",
+                  "alias": "Test",
+                  "email": "lis@groommang.onmicrosoft.com"
+              }
+          ],
+          "totalSaaS": 2,
+          "totalAlert": 0,
+          "totalDlp": 4,
+          "totalUser": 11,
+          "totalFile": 53,
+          "dailyFileCountDifference": 2,
+          "totalFileSize": 99131674,
+          "dailyFileSizeDifference": 50,
+          "fileSizeBySaaS": [
+              {
+                  "saas": "slack",
+                  "size": 98945945,
+                  "dailyDifference": 0
+              },
+              {
+                  "saas": "o365",
+                  "size": 185729,
+                  "dailyDifference": 50
+              }
+          ],
+          "fileUploadBySaaS": [
+              {
+                  "saas": "slack",
+                  "upload": 48,
+                  "dailyDifference": 0
+              },
+              {
+                  "saas": "o365",
+                  "upload": 5,
+                  "dailyDifference": 2
+              }
+          ]
+      }
     }
-    if(response.status == '200') {
-      return response.data;
-    }
+    return response
+    // const response = await api.get('/api/v1/main/total');
+    // if(response.data.status == 'error') {
+    //   router.push('/');
+    //   return;
+    // }
+    // if(response.status == '200') {
+    //   return response.data;
+    // }
   } catch (err) {
     throw err;  
   }
@@ -23,14 +73,112 @@ let mainTotalApi = async () => {
 
 let mainStatisticsApi = async () => {
   try {
-    const response = await api.get('/api/v1/main/statistics');
-    if(response.data.status == 'error') {
-      router.push('/');
-      return;
+    const response = {
+      "status": "success",
+      "data": {
+          "fileScanInToday": [
+              {
+                  "saas": "o365",
+                  "fileName": "유빈의 텍스트파일_공유.txt",
+                  "suspicious": 1,
+                  "dlp": 1,
+                  "vt": -1,
+                  "creator": "이 유빈",
+                  "eventTs": "2024-10-01T22:47:31"
+              },
+              {
+                  "saas": "o365",
+                  "fileName": "민수의 텍스트파일_공유.txt",
+                  "suspicious": 1,
+                  "dlp": 1,
+                  "vt": -1,
+                  "creator": "선 민수",
+                  "eventTs": "2024-10-01T22:39:08"
+              }
+          ],
+          "fileAnalysis": {
+              "totalCount": 53,
+              "suspiciousAnalysis": 43,
+              "dlpAnalysis": 30,
+              "vtAnalysis": 25
+          },
+          "fileHistoryInfo": [
+              {
+                  "saas": "slack",
+                  "upload": 73,
+                  "change": 9,
+                  "delete": 24,
+                  "lastActivity": "2024-10-02"
+              },
+              {
+                  "saas": "o365",
+                  "upload": 5,
+                  "change": 0,
+                  "delete": 0,
+                  "lastActivity": "2024-10-02"
+              }
+          ],
+          "fileHistoryStatistics": [
+              {
+                  "month": "2024-10",
+                  "total": 3
+              },
+              {
+                  "month": "2024-09",
+                  "total": 54
+              },
+              {
+                  "month": "2024-08",
+                  "total": 47
+              },
+              {
+                  "month": "2024-07",
+                  "total": 3
+              },
+              {
+                  "month": "2024-06",
+                  "total": 4
+              },
+              {
+                  "month": "2024-05",
+                  "total": 0
+              },
+              {
+                  "month": "2024-04",
+                  "total": 0
+              },
+              {
+                  "month": "2024-03",
+                  "total": 0
+              },
+              {
+                  "month": "2024-02",
+                  "total": 0
+              },
+              {
+                  "month": "2024-01",
+                  "total": 0
+              },
+              {
+                  "month": "2023-12",
+                  "total": 0
+              },
+              {
+                  "month": "2023-11",
+                  "total": 0
+              }
+          ]
+      }
     }
-    if(response.status == '200') {
-      return response.data;
-    }
+    return response
+    // const response = await api.get('/api/v1/main/statistics');
+    // if(response.data.status == 'error') {
+    //   router.push('/');
+    //   return;
+    // }
+    // if(response.status == '200') {
+    //   return response.data;
+    // }
   } catch (err) {
     throw err;  
   }
@@ -38,14 +186,66 @@ let mainStatisticsApi = async () => {
 
 let mainDlpApi = async () => {
   try {
-    const response = await api.get('/api/v1/main/dlp');
-    if(response.data.status == 'error') {
-      router.push('/');
-      return;
+    const response = {
+      "status": "success",
+      "data": {
+          "statisticsByPolicies": [
+              {
+                  "policy": "DLP 정책 테스트",
+                  "pii": [
+                      {
+                          "pii": "identify",
+                          "total": 4
+                      },
+                      {
+                          "pii": "passport",
+                          "total": 3
+                      },
+                      {
+                          "pii": "drive",
+                          "total": 2
+                      }
+                  ]
+              },
+              {
+                  "policy": "유빈 - TEST",
+                  "pii": [
+                      {
+                          "pii": "identify",
+                          "total": 4
+                      },
+                      {
+                          "pii": "passport",
+                          "total": 3
+                      }
+                  ]
+              }
+          ],
+          "statisticsByPiis": [
+              {
+                  "pii": "identify",
+                  "total": 44
+              },
+              {
+                  "pii": "passport",
+                  "total": 37
+              },
+              {
+                  "pii": "drive",
+                  "total": 36
+              }
+          ]
+      }
     }
-    if(response.status == '200') {
-      return response.data;
-    }
+    return response
+    // const response = await api.get('/api/v1/main/dlp');
+    // if(response.data.status == 'error') {
+    //   router.push('/');
+    //   return;
+    // }
+    // if(response.status == '200') {
+    //   return response.data;
+    // }
   } catch (err) {
     throw err;  
   }
