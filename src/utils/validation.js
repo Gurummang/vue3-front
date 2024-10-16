90b8cc5d843bc667535cff7a9b2820ec1d0ce6d5
SunM <coex0113@gmail.com>
2024-10-16 20:00:02 +0900

    KAN-540 Feat. 확장자가 Dlp와 관련된 것인지 판단 함수 구현

--- src/utils/validation.js
+++ src/utils/validation.js
@@ -3,4 +3,32 @@ export const validateEmail = (email) => {
   return re.test(String(email).toLowerCase());
 };
 
-// export { validateEmail };
+export const isDlpFile = (file) => {
+  const prohibitedMimes = [
+    'application/pdf',
+    'application/msword', // doc
+    'application/vnd.openxmlformats-officedocument.wordprocessingml.document', // docx
+    'application/vnd.ms-excel', // xls
+    'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet', // xlsx
+    'text/csv', // csv
+    'application/vnd.ms-powerpoint', // ppt
+    'application/vnd.openxmlformats-officedocument.presentationml.presentation', // pptx
+    'text/plain', // txt
+    'application/x-hwp', // hwp
+  ];
+  const prohibitedSignatures = [
+    'pdf',
+    'doc',
+    'docx',
+    'xls',
+    'xlsx',
+    'csv',
+    'ppt',
+    'pptx',
+    'txt',
+    'hwp'
+  ];
+
+  // MIME 타입이나 확장자 중 하나라도 일치하면
+  return prohibitedMimes.includes(file.mime) || prohibitedSignatures.includes(file.sign);
+}
