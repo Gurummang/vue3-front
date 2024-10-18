33ecba14b8f90bf94f93480e55f210506b87ddd1
SunM <coex0113@gmail.com>
2024-10-18 11:05:03 +0900

    Feat. 히스토리 시각화 - 각각 분리했었던 SaaS 하나로 묶어서 시각화하기

--- src/components/modals/HistoryVisualizationModal.vue
+++ src/components/modals/HistoryVisualizationModal.vue
@@ -66,19 +66,30 @@ const nodeTypes = {
 }
 
 const elements = computed(() => {
-  const slackData = data.data.slack.sort((a, b) => new Date(a.eventTs) - new Date(b.eventTs));
-  const googledriveData = data.data.googleDrive.sort((a, b) => new Date(a.eventTs) - new Date(b.eventTs));
-  const o365Data = data.data.o365.sort((a, b) => new Date(a.eventTs) - new Date(b.eventTs));
+  // const slackData = data.data.slack.sort((a, b) => new Date(a.eventTs) - new Date(b.eventTs))
+  // const googledriveData = data.data.googleDrive.sort((a, b) => new Date(a.eventTs) - new Date(b.eventTs))
+  // const o365Data = data.data.o365.sort((a, b) => new Date(a.eventTs) - new Date(b.eventTs))
 
   // 유사도에 따른 높이 랭크 설정
-  const slackSimilarities = [...new Set(slackData.map(item => item.similarity))].sort((a, b) => b - a)
-  const slackSimilarityRanks = Object.fromEntries(slackSimilarities.map((sim, index) => [sim, index + 1]))
+  // const slackSimilarities = [...new Set(slackData.map(item => item.similarity))].sort((a, b) => b - a)
+  // const slackSimilarityRanks = Object.fromEntries(slackSimilarities.map((sim, index) => [sim, index + 1]))
 
-  const googledriveSimilarities = [...new Set(googledriveData.map(item => item.similarity))].sort((a, b) => b - a)
-  const googledriveSimilarityRanks = Object.fromEntries(googledriveSimilarities.map((sim, index) => [sim, index + 1]))
+  // const googledriveSimilarities = [...new Set(googledriveData.map(item => item.similarity))].sort((a, b) => b - a)
+  // const googledriveSimilarityRanks = Object.fromEntries(googledriveSimilarities.map((sim, index) => [sim, index + 1]))
 
-  const o365Similarities = [...new Set(o365Data.map(item => item.similarity))].sort((a, b) => b - a)
-  const o365SimilarityRanks = Object.fromEntries(o365Similarities.map((sim, index) => [sim, index + 1]))
+  // const o365Similarities = [...new Set(o365Data.map(item => item.similarity))].sort((a, b) => b - a)
+  // const o365SimilarityRanks = Object.fromEntries(o365Similarities.map((sim, index) => [sim, index + 1]))
+
+  const allData = [
+    ...data.data.slack,
+    ...data.data.googleDrive,
+    ...data.data.o365
+  ]
+  .sort((a, b) => new Date(a.eventTs) - new Date(b.eventTs))
+
+  const allSimilarities = [...new Set(allData.map(item => item.similarity))].sort((a, b) => b - a)
+  const allSimilarityRanks = Object.fromEntries(allSimilarities.map((sim, index) => [sim, index + 1]))
+
 
   // 부모 노드 추가
   const nodes = [
@@ -97,34 +108,15 @@ const elements = computed(() => {
   ]
   const edges = []
 
-  slackData.forEach((item, index) => {
+  allData.forEach((item, index) => {
     const nodeId = `${item.eventId}`;
     let yPosition;
     let tuning = 0;
-    
-    // (1) 이벤트 행위에 따른 높이 구분
-    // switch(item.eventType.toLowerCase()) {
-    //   case 'file_upload':
-    //     // tuning = Math.floor((getSecureRandomInt(10) * (tuning - (-tuning))) + (-tuning));
-    //     tuning = 0;
-    //     break;
-    //   case 'file_change':
-    //     tuning = -100;
-    //     break;
-    //   case 'file_delete':
-    //     tuning = -200;
-    //     break;
-    //   default:
-    //     tuning = 0; // 기본값
-    // }
 
-    // (2) 유사도가 같은 것 높이 유지
-    // yPosition = Math.floor(-(100 - (item.similarity)) * 5)
-    // if(yPosition < 0) yPosition -= 300
-
-    // (3) 유사도에 따른 높이랭크를 설정
-    const similarityHeight = slackSimilarityRanks[item.similarity]
-    yPosition = -(similarityHeight * 230) + tuning;
+    // 유사도에 따른 높이랭크를 설정
+    const similarityHeight = allSimilarityRanks[item.similarity]
+    // yPosition = -(similarityHeight * 230) + tuning;
+    yPosition = -(similarityHeight * 150) + tuning;
 
     nodes.push({
       id: nodeId,
@@ -149,106 +141,158 @@ const elements = computed(() => {
     })
   })
 
-  googledriveData.forEach((item, index) => {
-    const nodeId = `${item.eventId}`;
-    let yPosition;
-    let tuning = 25;
+  // slackData.forEach((item, index) => {
+  //   const nodeId = `${item.eventId}`;
+  //   let yPosition;
+  //   let tuning = 0;
     
-    // (1) 이벤트 행위에 따른 높이 구분
-    // switch(item.eventType.toLowerCase()) {
-    //   case 'file_upload':
-    //     yPosition = Math.floor((getSecureRandomInt(100) * (tuning - (-tuning))) + (-tuning));
-    //     break;
-    //   case 'file_change':
-    //     yPosition = -300;
-    //     break;
-    //   case 'file_delete':
-    //     yPosition = -600;
-    //     break;
-    //   default:
-    //     yPosition = 0; // 기본값
-    // }
+  //   // (1) 이벤트 행위에 따른 높이 구분
+  //   // switch(item.eventType.toLowerCase()) {
+  //   //   case 'file_upload':
+  //   //     // tuning = Math.floor((getSecureRandomInt(10) * (tuning - (-tuning))) + (-tuning));
+  //   //     tuning = 0;
+  //   //     break;
+  //   //   case 'file_change':
+  //   //     tuning = -100;
+  //   //     break;
+  //   //   case 'file_delete':
+  //   //     tuning = -200;
+  //   //     break;
+  //   //   default:
+  //   //     tuning = 0; // 기본값
+  //   // }
 
-    // (2) 유사도가 같은 것 높이 유지
-    // yPosition = Math.floor(-(100 - (item.similarity)) * 5)
-    // if(yPosition < 0) yPosition -= 300
+  //   // (2) 유사도가 같은 것 높이 유지
+  //   // yPosition = Math.floor(-(100 - (item.similarity)) * 5)
+  //   // if(yPosition < 0) yPosition -= 300
 
-    // (3) 유사도에 따른 높이랭크를 설정
-    const similarityHeight = googledriveSimilarityRanks[item.similarity]
-    yPosition = -(similarityHeight * 230);
+  //   // (3) 유사도에 따른 높이랭크를 설정
+  //   const similarityHeight = slackSimilarityRanks[item.similarity]
+  //   yPosition = -(similarityHeight * 230) + tuning;
 
-    nodes.push({
-      id: nodeId,
-      type: 'custom',
-      data: { 
-        originNode : item.eventId === data.data.originNode,
-        eventType: item.eventType,
-        saas: item.saas,
-        fileName: item.fileName,
-        email: item.email,
-        eventTs: item.eventTs,
-        uploadChannel: item.uploadChannel,
-        similarity : item.similarity,
-        dlp : item.dlp,
-        threat : item.threat
-      },
-      targetPosition: Position.Left,
-      sourcePosition: Position.Right,
-      position: { x: 50 + index * 400, y: yPosition + 300},
-      // parentNode: item.saas,
-      // extent: 'parent',
-    })
-  })
+  //   nodes.push({
+  //     id: nodeId,
+  //     type: 'custom',
+  //     data: { 
+  //       originNode : item.eventId === data.data.originNode,
+  //       eventType: item.eventType,
+  //       saas: item.saas,
+  //       fileName: item.fileName,
+  //       email: item.email,
+  //       eventTs: item.eventTs,
+  //       uploadChannel: item.uploadChannel,
+  //       similarity : item.similarity,
+  //       dlp : item.dlp,
+  //       threat : item.threat
+  //     },
+  //     targetPosition: Position.Left,
+  //     sourcePosition: Position.Right,
+  //     position: { x: 50 + index * 400, y: yPosition },
+  //     // parentNode: item.saas,
+  //     // extent: 'parent',
+  //   })
+  // })
 
-  o365Data.forEach((item, index) => {
-    const nodeId = `${item.eventId}`;
-    let yPosition;
+  // googledriveData.forEach((item, index) => {
+  //   const nodeId = `${item.eventId}`;
+  //   let yPosition;
+  //   let tuning = 25;
     
-    // (1) 이벤트 행위에 따른 높이 구분
-    // switch(item.eventType.toLowerCase()) {
-    //   case 'file_upload':
-    //     yPosition = Math.floor((getSecureRandomInt(100) * (tuning - (-tuning))) + (-tuning));
-    //     break;
-    //   case 'file_change':
-    //     yPosition = -300;
-    //     break;
-    //   case 'file_delete':
-    //     yPosition = -600;
-    //     break;
-    //   default:
-    //     yPosition = 0; // 기본값
-    // }
+  //   // (1) 이벤트 행위에 따른 높이 구분
+  //   // switch(item.eventType.toLowerCase()) {
+  //   //   case 'file_upload':
+  //   //     yPosition = Math.floor((getSecureRandomInt(100) * (tuning - (-tuning))) + (-tuning));
+  //   //     break;
+  //   //   case 'file_change':
+  //   //     yPosition = -300;
+  //   //     break;
+  //   //   case 'file_delete':
+  //   //     yPosition = -600;
+  //   //     break;
+  //   //   default:
+  //   //     yPosition = 0; // 기본값
+  //   // }
 
-    // (2) 유사도가 같은 것 높이 유지
-    // yPosition = Math.floor(-(100 - (item.similarity)) * 5)
-    // if(yPosition < 0) yPosition -= 300
+  //   // (2) 유사도가 같은 것 높이 유지
+  //   // yPosition = Math.floor(-(100 - (item.similarity)) * 5)
+  //   // if(yPosition < 0) yPosition -= 300
 
-    // (3) 유사도에 따른 높이랭크를 설정
-    const similarityHeight = o365SimilarityRanks[item.similarity]
-    yPosition = -(similarityHeight * 230);
+  //   // (3) 유사도에 따른 높이랭크를 설정
+  //   const similarityHeight = googledriveSimilarityRanks[item.similarity]
+  //   yPosition = -(similarityHeight * 230);
 
-    nodes.push({
-      id: nodeId,
-      type: 'custom',
-      data: { 
-        originNode : item.eventId === data.data.originNode,
-        eventType: item.eventType,
-        saas: item.saas,
-        fileName: item.fileName,
-        email: item.email,
-        eventTs: item.eventTs,
-        uploadChannel: item.uploadChannel,
-        similarity : item.similarity,
-        dlp : item.dlp,
-        threat : item.threat
-      },
-      targetPosition: Position.Left,
-      sourcePosition: Position.Right,
-      position: { x: 50 + index * 400, y: yPosition + 600},
-      // parentNode: item.saas,
-      // extent: 'parent',
-    })
-  })
+  //   nodes.push({
+  //     id: nodeId,
+  //     type: 'custom',
+  //     data: { 
+  //       originNode : item.eventId === data.data.originNode,
+  //       eventType: item.eventType,
+  //       saas: item.saas,
+  //       fileName: item.fileName,
+  //       email: item.email,
+  //       eventTs: item.eventTs,
+  //       uploadChannel: item.uploadChannel,
+  //       similarity : item.similarity,
+  //       dlp : item.dlp,
+  //       threat : item.threat
+  //     },
+  //     targetPosition: Position.Left,
+  //     sourcePosition: Position.Right,
+  //     position: { x: 50 + index * 400, y: yPosition + 300},
+  //     // parentNode: item.saas,
+  //     // extent: 'parent',
+  //   })
+  // })
+
+  // o365Data.forEach((item, index) => {
+  //   const nodeId = `${item.eventId}`;
+  //   let yPosition;
+    
+  //   // (1) 이벤트 행위에 따른 높이 구분
+  //   // switch(item.eventType.toLowerCase()) {
+  //   //   case 'file_upload':
+  //   //     yPosition = Math.floor((getSecureRandomInt(100) * (tuning - (-tuning))) + (-tuning));
+  //   //     break;
+  //   //   case 'file_change':
+  //   //     yPosition = -300;
+  //   //     break;
+  //   //   case 'file_delete':
+  //   //     yPosition = -600;
+  //   //     break;
+  //   //   default:
+  //   //     yPosition = 0; // 기본값
+  //   // }
+
+  //   // (2) 유사도가 같은 것 높이 유지
+  //   // yPosition = Math.floor(-(100 - (item.similarity)) * 5)
+  //   // if(yPosition < 0) yPosition -= 300
+
+  //   // (3) 유사도에 따른 높이랭크를 설정
+  //   const similarityHeight = o365SimilarityRanks[item.similarity]
+  //   yPosition = -(similarityHeight * 230);
+
+  //   nodes.push({
+  //     id: nodeId,
+  //     type: 'custom',
+  //     data: { 
+  //       originNode : item.eventId === data.data.originNode,
+  //       eventType: item.eventType,
+  //       saas: item.saas,
+  //       fileName: item.fileName,
+  //       email: item.email,
+  //       eventTs: item.eventTs,
+  //       uploadChannel: item.uploadChannel,
+  //       similarity : item.similarity,
+  //       dlp : item.dlp,
+  //       threat : item.threat
+  //     },
+  //     targetPosition: Position.Left,
+  //     sourcePosition: Position.Right,
+  //     position: { x: 50 + index * 400, y: yPosition + 600},
+  //     // parentNode: item.saas,
+  //     // extent: 'parent',
+  //   })
+  // })
 
 // Edges 생성
   data.data.edges.forEach((item) => {
