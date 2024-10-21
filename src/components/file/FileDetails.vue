128b79db5f489950d9c5a4059af895a11cb0bd16
SunM <coex0113@gmail.com>
2024-10-21 15:23:56 +0900

    Feat. 파일 검사 - totalCount. 표시 및 필터링 시 페이지 수 1로 설정

--- src/components/file/FileDetails.vue
+++ src/components/file/FileDetails.vue
@@ -2,7 +2,7 @@
   <div>
     <div class="bg-white shadow-sm rounded-lg p-4 mb-5">
       <div class="mb-2">
-        <h2 class="text-xl font-bold text-gray-800 mb-4">파일 검사
+        <h2 class="text-xl font-bold text-gray-800 mb-4">파일 검사 - {{ totalCount }}개
           
         </h2>
         <div class="flex">
@@ -43,8 +43,8 @@
             </select>
 
             <div class="relative max-w-sm">
-              <input v-model="searchFilter" @keyup.enter="getData" class="w-full py-2 px-4 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-1 focus:ring-blue-500 focus:border-blue-500" type="search" placeholder="검색">
-              <button @click="getData" class="absolute inset-y-0 right-0 flex items-center px-4 text-gray-700 bg-gray-100 border border-gray-300 rounded-r-md hover:bg-gray-200 focus:outline-none focus:ring-1 focus:ring-blue-500 focus:border-blue-500">
+              <input v-model="searchFilter" @keyup.enter="() => { getData(); selectPages = 1; }" class="w-4/5 py-2 px-4 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-1 focus:ring-blue-500 focus:border-blue-500" type="search" placeholder="검색">
+              <button @click="() => { getData(); selectPages = 1; }" class="absolute inset-y-0 right-0 flex items-center px-4 text-gray-700 bg-gray-100 border border-gray-300 rounded-r-md hover:bg-gray-200 focus:outline-none focus:ring-1 focus:ring-blue-500 focus:border-blue-500">
               <svg class="h-5 w-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                 <path fill-rule="evenodd" clip-rule="evenodd" d="M14.795 13.408l5.204 5.204a1 1 0 01-1.414 1.414l-5.204-5.204a7.5 7.5 0 111.414-1.414zM8.5 14A5.5 5.5 0 103 8.5 5.506 5.506 0 008.5 14z" />
               </svg>
